c7438be8b7e39130c2ae2b7c14a0b9cbf22e7bb3
Jos de Jong <wjosdejong@gmail.com>
2012-09-18 11:03:45 +0200

    Fixed persisting an agents context in case of a nested call to itself.

--- java/src/EveGoogleAppEngine/src/com/almende/eve/context/google/DatastoreContext.java
+++ java/src/EveGoogleAppEngine/src/com/almende/eve/context/google/DatastoreContext.java
@@ -319,6 +319,7 @@ public class DatastoreContext implements Context {
 	@Override
 	public synchronized void destroy() {
 		if (isChanged) {
+			refresh();
 			saveToDatastore();
 			isChanged = false;
 		}
