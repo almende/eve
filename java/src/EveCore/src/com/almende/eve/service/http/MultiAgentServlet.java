1e5285100a04eefc4170705827b7748c593614b3
josdejong <wjosdejong@gmail.com>
2012-12-05 20:15:43 +0100

    Integrated agent management (create, delete) in the MultiAgentServlet instead of a separate ManagementServlet.

--- java/src/EveCore/src/com/almende/eve/service/http/MultiAgentServlet.java
+++ java/src/EveCore/src/com/almende/eve/service/http/MultiAgentServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.almende.eve.agent.Agent;
 import com.almende.eve.agent.AgentFactory;
 import com.almende.eve.config.Config;
 import com.almende.eve.json.JSONRPCException;
@@ -34,16 +35,26 @@ public class MultiAgentServlet extends HttpServlet {
 		}
 	}
 	
+	/**
+	 * Get an agents web interface
+	 * Usage: GET /servlet/{agentId}
+	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		String uri = req.getRequestURI();
 		String agentId = httpService.getAgentId(uri);
 		String resource = httpService.getAgentResource(uri);
+
+		// if no agentId is found, return generic information on servlet usage
+		if (agentId == null || agentId.isEmpty()) {
+			resp.getWriter().write(getServletDocs());
+			return;
+		}
 		
 		// check if the agent exists
 		try {
-			if (agentId == null || agentId.isEmpty() || !agentFactory.hasAgent(agentId)) {
+			if (!agentFactory.hasAgent(agentId)) {
 				resp.sendError(404, "Agent with id '" + agentId + "' not found.");
 				return;
 			}
@@ -76,6 +87,12 @@ public class MultiAgentServlet extends HttpServlet {
 		}
 	}
 
+	/**
+	 * Send a JSON-RPC message to an agent
+	 * Usage: POST /servlet/{agentId}
+	 *        With a JSON-RPC request as body.
+	 *        Response will be a JSON-RPC response.
+	 */
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
@@ -92,8 +109,9 @@ public class MultiAgentServlet extends HttpServlet {
 			// invoke the agent
 			agentUrl = req.getRequestURI();
 			agentId = httpService.getAgentId(agentUrl);
-			if (agentId == null) {
-				throw new Exception("Agent with id '" + agentId + "' not found.");
+			if (agentId == null || agentId.isEmpty()) {
+				resp.sendError(400, "No agentId found in url.");
+				return;
 			}
 			jsonResponse = agentFactory.invoke(agentId, jsonRequest);
 		} catch (Exception err) {
@@ -114,6 +132,62 @@ public class MultiAgentServlet extends HttpServlet {
 		resp.addHeader("Content-Type", "application/json");
 		resp.getWriter().println(jsonResponse.toString());
 		resp.getWriter().close();
+	}
+
+	/**
+	 * Create a new agent
+	 * Usage: PUT /servlet/{agentId}?class={agentClass}
+	 *        Where agentClass is the full class path of the agent.
+	 *        Returns a list with the urls of the created agent.
+	 */
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		String agentUrl = req.getRequestURI();
+		String agentId = httpService.getAgentId(agentUrl);
+		String agentClass = req.getParameter("class");
+		
+		if (agentId == null) {
+			resp.sendError(400, "No agentId found in url.");
+			return;
+		}
+		if (agentClass == null || agentClass.isEmpty()) {
+			resp.sendError(400, "Query parameter 'class' missing in url.");
+			return;
+		}
+		
+		try {
+			Agent agent = agentFactory.createAgent(agentClass, agentId);
+			for (String url : agent.getUrls()) {
+				resp.getWriter().println(url);
+			}
+			agent.destroy();
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+
+	/**
+	 * Delete an agent
+	 * usage: DELETE /servlet/agentId
+	 */
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		String agentUrl = req.getRequestURI();
+		String agentId = httpService.getAgentId(agentUrl);
+
+		if (agentId == null) {
+			resp.sendError(400, "No agentId found in url.");
+			return;
+		}
+		
+		try {
+			agentFactory.deleteAgent(agentId);
+			resp.getWriter().write("Agent " + agentId + " deleted");
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
 	}
 	
 	/**
@@ -143,5 +217,54 @@ public class MultiAgentServlet extends HttpServlet {
 		
 		// TODO: this will not work with multiple http servlets
 		httpService = (HttpService) agentFactory.getService("http");
+	}
+	
+	
+	/**
+	 * Get a description on how to use this servlet
+	 * @return info
+	 */
+	private String getServletDocs() {
+		String servletUrl = httpService.getServletUrl();
+		String info = 
+			"EVE AGENTS SERVLET\n" +
+			"\n" +
+			"Usage:\n" +
+			"\n" +
+			
+			"GET " + servletUrl + "\n" +
+			"\n" +
+			"    Returns information on how to use this servlet.\n" +
+			"\n" +
+			
+			"GET " + servletUrl + "{agentId}\n" +
+			"\n" +
+			"    Returns an agents web interface, allowing for easy interaction\n" +
+			"    with the agent.\n" +
+			"    A 404 error will be returned when the agent does not exist.\n" +
+			"\n" +
+			
+			"POST " + servletUrl + "{agentId}\n" +
+			"\n" +
+			"    Send an RPC call to an agent.\n" +
+			"    The body of the request must contain a JSON-RPC request.\n" +
+			"    The addressed agent will execute the request and return a\n" +
+			"    JSON-RPC response. This response can contain the result or\n" +
+			"    an exception.\n" +
+			"    A 404 error will be returned when the agent does not exist.\n" +
+			"\n" +
+			
+			"PUT " + servletUrl + "{agentId}?class={agentClass}\n" +
+			"\n" +
+			"    Create an agent. agentId can be any string. agentClass must\n" +
+			"    be a full java class path of an Agent. A 500 error will be\n" +
+			"    thrown when an agent with this id already exists.\n" +
+			"\n" +
+
+			"DELETE " + servletUrl + "{agentId}\n" +
+			"\n" +
+			"    Delete an agent by its id.";
+
+		return info;
 	}
 }
