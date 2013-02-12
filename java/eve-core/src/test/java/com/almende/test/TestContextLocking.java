3e951871b2a58149bc3c0e8fa6b2de96dd841fdd
Ludo Stellingwerff <ludo@almende.org>
2013-02-12 15:56:21 +0100

    Make the test somewhat more stringent..

--- java/eve-core/src/test/java/com/almende/test/TestContextLocking.java
+++ java/eve-core/src/test/java/com/almende/test/TestContextLocking.java
@@ -14,6 +14,9 @@ import com.almende.eve.context.Context;
 import com.almende.eve.context.FileContext;
 
 public class TestContextLocking extends TestCase {
+	//TODO: prove that a collision occurs, possibly by measuring the starttime and runtime of each run.
+	//TODO: alternatively: implement a non-locking, non-thread-safe version of the context and see it break:)
+	
 	
 	private void testRun(final Context context){
 		//context.clear();
@@ -25,24 +28,53 @@ public class TestContextLocking extends TestCase {
 			@Override
 			public void run() {
 				context.put("test","test1");
+				context.put("test","test1");
+				context.get("test");
+				context.put("test1","test");
+				context.get("test1");
 			}
 			
-		}, 0, 300, TimeUnit.MILLISECONDS);
-		final ScheduledFuture<?> thread2 = scheduler.scheduleAtFixedRate(new Runnable(){
+		}, 0, 100, TimeUnit.MILLISECONDS);
+		final ScheduledFuture<?> thread2 = scheduler.scheduleWithFixedDelay(new Runnable(){
 			@Override
 			public void run() {
 				context.put("test","test2");
+				context.put("test","test2");
+				context.get("test");
+				context.put("test1","test");
+				context.put("test1","test");
+				context.get("test1");
 			}
 			
-		}, 100, 100, TimeUnit.MILLISECONDS);
+		}, 110, 95, TimeUnit.MILLISECONDS);
+		final ScheduledFuture<?> thread3 = scheduler.scheduleWithFixedDelay(new Runnable(){
+			@Override
+			public void run() {
+				context.put("test","test3");
+				context.put("test","test3");
+				context.get("test");
+				context.put("test1","test");
+				context.put("test1","test");
+				context.get("test1");
+			}
+			
+		}, 105, 97, TimeUnit.MILLISECONDS);
 		scheduler.schedule(new Runnable(){
 
 			@Override
 			public void run() {
 				thread1.cancel(false);
 				thread2.cancel(false);
+				thread3.cancel(false);
 			}
-		}, 1200, TimeUnit.MILLISECONDS);
+		}, 1450, TimeUnit.MILLISECONDS);
+		long start = System.currentTimeMillis();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			System.out.println("Sleep interrupted after:"+(System.currentTimeMillis()-start)+" ms.");
+		}
+		assertEquals("test",(String)context.get("test1"));
 		assertEquals("test2",(String)context.get("test2"));
 		assertTrue(((String)context.get("test")).startsWith("test"));
 		
