89023ef3585b0d2851115557bf3a8a9696b17140
josdejong <wjosdejong@gmail.com>
2013-09-05 17:36:34 +0200

    Improved menu

--- js/menu.js
+++ js/menu.js
@@ -3,48 +3,44 @@
  http://jasalguero.com/ledld/development/web/expandable-list/
  */
 
-var loaded = false;
-
 function prepareMenu() {
     var menuTree = $('#menu-tree');
 
     // make all expandable list items clickable
-    menuTree.find('li:has(ul)')
+    menuTree.find('.title')
         .click( function(event) {
             if (this == event.target) {
-                $(this).toggleClass('expanded');
-                $(this).children('ul').toggle(loaded ? 400: 0);
+                var li = $(this).parent();
+                toggleExpanded(li, 400);
 
                 storeExpanded();
 
                 return false;
             }
         })
+        .parent()
         .addClass('collapsed')
         .children('ul').hide();
 
     // restore the last state: expanded.collapsed items
     loadExpanded();
 
-    // find the page where we are now
+    // find the page where we are now and highlight it
     menuTree.find('a').each(function (index, elem) {
         if (location.href.indexOf(elem.href) == 0) {
-            // highlight it
-            var parent =  $(elem).parent();
+            var parent = $(elem).parent();
             parent.addClass('current');
 
             // expand all elements to this path
             while (parent[0]) {
                 if (parent.hasClass('collapsed') && !parent.hasClass('expanded')) {
-                    parent.click();
+                    toggleExpanded(parent, 0);
                 }
 
                 parent = parent.parent();
             }
         }
     });
-
-    loaded = true;
 }
 
 // store the expanded menu items in local storage
@@ -68,15 +64,22 @@ function loadExpanded() {
         if (window['localStorage']) {
             var expanded = ['concepts']; // default
             if (localStorage['expanded']) {
-                var expanded = JSON.parse(localStorage['expanded']);
+                expanded = JSON.parse(localStorage['expanded']);
             }
 
             expanded.forEach(function (id) {
-                $('#' + id).click();
+                var li = $('#' + id);
+                toggleExpanded(li, 0);
             });
         }
     }
     catch(e) {}
+}
+
+function toggleExpanded (elem, duration) {
+    elem.toggleClass('expanded')
+        .children('ul')
+        .toggle(duration)
 }
 
 $(document).ready( function() {
