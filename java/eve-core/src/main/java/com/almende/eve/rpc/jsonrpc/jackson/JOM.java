3a785337133e1bbce7d3e6435db7a268d40fb078
Ludo Stellingwerff <ludo@almende.org>
2013-09-30 18:18:45 +0200

    Reintroduce deprecated version of old unnecessary methods

--- java/eve-core/src/main/java/com/almende/eve/rpc/jsonrpc/jackson/JOM.java
+++ java/eve-core/src/main/java/com/almende/eve/rpc/jsonrpc/jackson/JOM.java
@@ -6,6 +6,7 @@ package com.almende.eve.rpc.jsonrpc.jackson;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.NullNode;
@@ -14,12 +15,13 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 public final class JOM {
-	private static final ObjectMapper m; 
+	private static final ObjectMapper	m;
 	static {
 		m = createInstance();
 	}
 	
-	protected JOM() {}
+	protected JOM() {
+	}
 	
 	public static ObjectMapper getInstance() {
 		return m;
@@ -37,12 +39,14 @@ public final class JOM {
 		return NullNode.getInstance();
 	}
 	
-	private static synchronized ObjectMapper createInstance () {
+	private static synchronized ObjectMapper createInstance() {
 		ObjectMapper mapper = new ObjectMapper();
 		
 		// set configuration
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL , false);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+				false);
+		mapper.configure(
+				DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, false);
 		
 		// Needed for o.a. JsonFileState
 		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
@@ -50,10 +54,29 @@ public final class JOM {
 		
 		mapper.registerModule(new JodaModule());
 		
-		
 		return mapper;
-	}	
-	public static TypeFactory getTypeFactory(){ 
+	}
+	
+	public static TypeFactory getTypeFactory() {
 		return m.getTypeFactory();
+	}
+	
+	/**
+	 * @deprecated This method is no longer needed, you can directly use Void.class
+	 * @return
+	 */
+	@Deprecated
+	public static JavaType getVoid() {
+		return m.getTypeFactory()
+				.uncheckedSimpleType(Void.class);
+	}
+	
+	/**
+	 * @deprecated This method is no longer needed, you can directly use <Class>.class, e.g. String.class
+	 * @return
+	 */
+	@Deprecated
+	public static JavaType getSimpleType(Class<?> c) {
+		return m.getTypeFactory().uncheckedSimpleType(c);
 	}
 }
