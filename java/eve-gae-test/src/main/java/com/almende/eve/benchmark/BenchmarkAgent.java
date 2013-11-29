06cdb19a82e1e466cc2dbb524f71fa2be626a65c
josdejong <wjosdejong@gmail.com>
2013-11-29 11:21:07 +0100

    moved HttpUtil to eve-planning (it's no longer used by eve-core)

---
+++ java/eve-gae-test/src/main/java/com/almende/eve/benchmark/BenchmarkAgent.java
@@ -0,0 +1,153 @@
+package com.almende.eve.benchmark;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.Date;
+
+import com.almende.eve.agent.Agent;
+import com.almende.eve.agent.AgentHost;
+import com.almende.eve.rpc.annotation.Access;
+import com.almende.eve.rpc.annotation.AccessType;
+import com.almende.eve.rpc.annotation.Name;
+import com.almende.eve.rpc.annotation.Required;
+import com.almende.eve.rpc.jsonrpc.JSONRPCException;
+import com.almende.eve.rpc.jsonrpc.JSONRequest;
+import com.almende.eve.state.State;
+import com.almende.eve.transport.TransportService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * BenchmarAgent can set up a test with a set of agents, and test performance
+ * of these agents interacting with each other.
+ * {@link BenchmarkAgent}
+ */
+@Access(AccessType.PUBLIC)
+public class BenchmarkAgent extends Agent {
+	@Override
+	public void create () {
+		getState().put("status", "none");
+	}
+	
+	public void startAsTask (@Required(true) @Name("num") Integer num) {
+		JSONRequest task = new JSONRequest();
+		task.setMethod("start");
+		ObjectNode params = new ObjectMapper().createObjectNode();
+		params.put("num", num);
+		task.setParams(params);
+		long delay = 0;
+		getScheduler().createTask(task, delay);
+	}
+	
+	/**
+	 * Start a new benchmark with the specified number of agents
+	 * @param num		Number of agents
+	 * @throws IOException 
+	 * @throws JSONRPCException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public ObjectNode start (@Required(true) @Name("num") Integer num) {
+		// cancel any currently running test
+		cancel();
+		
+		State state = getState();
+		AgentHost host = getAgentHost();
+		TransportService service = host.getTransportService("http");
+		
+		// create the required number of agents
+		for (int i = 0; i < num; i++) {
+			try {
+				String id = HelloAgent.numToAgentId(i, num);
+				host.createAgent(HelloAgent.class, id);
+			} catch (Exception e) {
+				e.printStackTrace();
+				// TODO: handle error
+			}
+		}
+		int pairs = num * (num - 1) / 2;
+		int messages = num * (num - 1);
+		state.put("num", num);
+		state.put("pairs", pairs);
+		state.put("messages", messages);
+		state.remove("duration");
+		state.put("status", "running");
+
+		Date start = new Date();
+		
+		// give the agents a start command
+		ObjectMapper mapper = new ObjectMapper();
+		for (int i = 0; i < num; i++) {
+			try {
+				String agentId = HelloAgent.numToAgentId(i, num);
+				URI url = new URI(service.getAgentUrl(agentId));
+				String method = "run";
+				ObjectNode params = mapper.createObjectNode();
+				params.put("num", num);
+				send(url, method, params);
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+		}
+
+		Date end = new Date();
+		state.put("duration", (end.getTime() - start.getTime())); // in millisec
+		state.put("status", "finished");
+
+		return status();
+	}
+
+	/**
+	 * Stop a running test
+	 */
+	public void cancel () {
+		State state = getState();
+		AgentHost host = getAgentHost();
+		
+		Integer num = state.get("num", Integer.class);
+		
+		// delete the agents
+		if (num != null) {
+			for (int i = 0; i < num; i++) {
+				try {
+					String id = HelloAgent.numToAgentId(i, num);
+					host.deleteAgent(id);
+				} catch (Exception e) {
+					e.printStackTrace();
+					// TODO: handle error
+				}
+			}
+		}
+		
+		state.put("num", 0);
+		state.put("status", "cancelled");
+	}
+	
+	/**
+	 * Get the status of the test
+	 * @return status 	A JSON object containing properties: 
+	 *                  {String} status
+	 *                  {Integer} num
+	 */
+	public ObjectNode status () {
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode status = mapper.createObjectNode();
+		
+		status.put("status", getState().get("status", String.class));
+		status.put("num", getState().get("num", Integer.class));
+		status.put("pairs", getState().get("pairs", Integer.class));
+		
+		Integer messages = getState().get("messages", Integer.class);
+		Long duration = getState().get("duration", Long.class);
+		status.put("messages", messages);
+		status.put("duration", duration);
+		if (messages != null & duration != null) {
+			status.put("durationPerMessage", (double)duration / (double)messages);
+		}
+		
+		return status;
+	}
+}
---
+++ java/eve-gae-test/src/main/java/com/almende/eve/benchmark/HelloAgent.java
@@ -0,0 +1,96 @@
+package com.almende.eve.benchmark;
+
+import java.net.URI;
+
+import com.almende.eve.agent.Agent;
+import com.almende.eve.rpc.annotation.Access;
+import com.almende.eve.rpc.annotation.AccessType;
+import com.almende.eve.rpc.annotation.Name;
+import com.almende.eve.rpc.annotation.Required;
+import com.almende.eve.state.State;
+import com.almende.eve.transport.TransportService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@Access(AccessType.PUBLIC)
+public class HelloAgent extends Agent {
+	private int n = 0; // the number of this agent
+
+	@Override
+	public void init() {
+		String id = getId(); 
+		n = agentIdToNum(id);
+	}	
+	
+	public int ping (@Required(true) @Name("counter") int counter) {
+		return counter + 1;
+	}
+	
+	/**
+	 * Send a request and response to each of the other agents
+	 * @param num  The total number of agents
+	 */
+	public void run (@Required(true) @Name("num") int num) {
+		State state = getState();
+		TransportService service = getAgentHost().getTransportService("http");
+		ObjectMapper mapper = new ObjectMapper();
+		
+		state.put("status", "running");
+		Integer counter = 0;
+		for (int i = 0; i < num; i++) {
+			if (i != n) {
+				try {
+					String agentId = numToAgentId(i, num);
+					URI url = new URI(service.getAgentUrl(agentId));
+					String method = "ping";
+					ObjectNode params = mapper.createObjectNode();
+					params.put("counter", counter);
+					counter = send(url, method, params, Integer.class);
+				} catch (Exception e) {
+					//e.printStackTrace();
+				}
+			}
+		}
+		
+		state.put("counter", counter);
+		state.put("status", "done");
+	}
+	
+	/**
+	 * Format an agentId, for example when n = 7 and num = 100,
+	 * the returned agentId will be "helloagent007"
+	 * @param n
+	 * @param num
+	 * @return agentId
+	 */
+	public static String numToAgentId (int n, int num) {
+		String format = "helloagent%0" + (int)Math.ceil(Math.log10(num)) + "d";
+		return String.format(format, n);
+	}
+
+	/**
+	 * Retrieve the number from an agentId, for example when 
+	 * agentId = "helloagent007", the returned n is 7 
+	 * @param agentId
+	 * @return n
+	 */
+	public static int agentIdToNum (String agentId) {
+		return Integer.parseInt(agentId.substring("helloagent".length()));
+	}
+	
+	/**
+	 * Get the status of the test
+	 * @return status 	A JSON object containing properties: 
+	 *                  {String} status
+	 *                  {Integer} num
+	 */
+	public ObjectNode status () {
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode status = mapper.createObjectNode();
+		
+		status.put("status", getState().get("status", String.class));
+		status.put("counter", getState().get("counter", Integer.class));
+		
+		return status;
+	}
+}
